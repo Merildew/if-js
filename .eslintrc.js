dbec7a21ce53a38f8b25e89d4e36806d19cd735d
Merildew <73027852+Merildew@users.noreply.github.com>
2021-09-05 10:54:26 +0300

    added prettier usage

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": ["eslint:recommended", "google"],
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "rules": {
-        "semi": ["error", "always"]
-    }
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "google", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    semi: ["error", "always"],
+    "require-jsdoc": 0,
+  },
 };
