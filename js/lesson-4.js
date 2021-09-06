b88324ef5365e09eb13dfe8fb528e6d2703403ed
Merildew <73027852+Merildew@users.noreply.github.com>
2021-09-06 13:33:08 +0300

    added 2 variant and some comments

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -8,16 +8,24 @@ console.log(sum(5)(2));
 
 // colored text task
 const colors = ["magenta", "cyan", "firebrick", "springgreen", "skyblue"];
+
+// with id-usage
 const element1 = document.getElementById("text1");
 const element2 = document.getElementById("text2");
 const element3 = document.getElementById("text3");
 const pBlock = [element1, element2, element3];
-console.log(pBlock);
 
 for (let i = 0; i < pBlock.length; i++) {
   pBlock[i].addEventListener("click", changeColor(pBlock[i]));
 }
 
+// with p-tag-usage
+const pArray = document.getElementsByTagName("p");
+for (let i = 0; i < pArray.length; i++) {
+  pArray[i].addEventListener("click", changeColor(pArray[i]));
+}
+
+// without this, because eslint dont like it here
 function changeColor(elem) {
   let counter = 0;
   return function () {
