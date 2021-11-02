b488cc4d0b4d18e8dae59efe5d52c8a16b9ce821
Merildew <73027852+Merildew@users.noreply.github.com>
2021-11-03 01:12:39 +0300

    complete lesson-12 task

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-$(document).ready(function () {
-  $(".homes-panel").slick({
-    slidesToShow: 4,
-    responsive: [
-      {
-        breakpoint: 1025,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 769,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-    ],
-  });
-});
-
 const data = [
   {
     name: "Hotel Leopold",
@@ -79,16 +59,47 @@ const data = [
 
 const homesInfoPanel = document.querySelector(".homes-panel");
 
+fillSlider();
+
+function fillSlider() {
+  fetch("https://fe-student-api.herokuapp.com/api/hotels/popular")
+    .then((response) =>
+      response.json().then((hotelArr) => {
+        insertFunc(data);
+        insertFunc(hotelArr);
+      })
+    )
+    .then(() => $(document).ready(slickSlider));
+}
+
 function insertFunc(arr) {
   arr.map((item) => {
     const homesItem = document.createElement("div");
     homesItem.classList.add("homes-item");
     homesItem.innerHTML = `
-      <img src="${item.imageUrl}" alt="${item.name}">
+      <img src="${item.imageUrl}" alt="${item.name}" class="homes-img">
       <a class="homes-link" href="#">${item.name}</a>
       <span class="homes-place">${item.city}, ${item.country}</span>`;
     homesInfoPanel.append(homesItem);
   });
 }
 
-insertFunc(data);
+function slickSlider() {
+  $(".homes-panel").slick({
+    slidesToShow: 4,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  });
+}
